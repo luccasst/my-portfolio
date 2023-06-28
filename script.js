bf7c9b4ceb3bb3a291a40f674946dbd31a9f7eb9
luccasst <lucas.c.r.f@hotmail.com>
2023-06-28 17:56:13 -0300

    add section from project

--- script.js
+++ script.js
@@ -53,3 +53,42 @@ $(document).ready(function(){
         }
     });
 });
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+    slider.forEach(item => {
+        item.style.opacity = 0;
+        item.style.zIndex = -1;
+      });}
+
+function showSlider() {
+    slider[currentSlide].style.opacity = 1;
+    slider[currentSlide].style.zIndex = 1;}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
